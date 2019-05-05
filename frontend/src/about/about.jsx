260a410bfeb1b1a73266adc54c9610688131ab1a
Danilo Valério da Silva <danilo_vs@hotmail.com>
2019-05-05 09:22:50 -0300

    PageHeader Component

--- frontend/src/about/about.jsx
+++ frontend/src/about/about.jsx
@@ -1,8 +1,17 @@
 import React, {Component} from 'react'
+import PageHeader from '../template/pageHeader'
+
 //Componente funcional
 
 export default props => (
     <div>
-        <h1>Sobre</h1>
+        <PageHeader name='Sobre' small='Nós'/>
+        <h3>Nossa história</h3>
+        <p>Blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá </p>
+        <h3>Missão visão e valores</h3>
+        <p>Blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá </p>
+        <h3>Divulgação</h3>
+        <p>Blá blá blá blá blá blá blá blá blá blá blá blá blá blá blá </p>
+
     </div>
 )
