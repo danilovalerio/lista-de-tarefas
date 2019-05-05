b069814bf146c29f8aa6b1c36ac9085e725ce306
Danilo Valério da Silva <danilo_vs@hotmail.com>
2019-05-05 09:28:20 -0300

    Formulario e Lista Components

---
+++ frontend/src/todo/todoForm.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default props => (
+    <di>
+        <h1>Form</h1>
+    </di>
+)
---
+++ frontend/src/todo/todoList.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default props => (
+    <di>
+        <h1>Lista</h1>
+    </di>
+)
