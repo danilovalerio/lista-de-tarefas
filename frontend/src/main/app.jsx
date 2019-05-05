40016cab152c6671c3c687d671473ea715cfb92d
Danilo Valério da Silva <danilo_vs@hotmail.com>
2019-05-05 01:39:10 -0300

    Autalização e correções no webpack

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -1,16 +1,12 @@
 import 'modules/bootstrap/dist/css/bootstrap.min.css'
 import 'modules/font-awesome/css/font-awesome.min.css'
-import '../template/custom.css'
+// import '../template/custom.css'
 
 import React from 'react'
-// import Menu from '../template/menu'
-// import Routes from './routes'
 
 export default props => (
-    <div className='container'>
-        {/* <Menu />
-        <Routes /> */}
-        <h1>Teste</h1>
+  <div className="container">
+      <h1>Teste Componente App</h1>
+  </div>
 
-    </div>
 )
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,23 +1,23 @@
 const webpack = require('webpack')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
-//entry é o nosso entrypoint
 module.exports = {
     entry: './src/index.jsx',
     output: {
-        path:__dirname + '/public',
+        path: __dirname + '/public',
         filename: './app.js'
     },
     devServer: {
         port: 8080,
         contentBase: './public',
-
     },
-    resolve: ['','.js','.jsx'],
-    alias: {
-        modules: __dirname + '/node_modules'
+    resolve: {
+        extensions: ['', '.js', '.jsx'],
+        alias: {
+            modules: __dirname + '/node_modules'
+        }
     },
-    plugins: [
+    plugins: [ 
         new ExtractTextPlugin('app.css')
     ],
     module: {
