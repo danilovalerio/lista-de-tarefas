6947b826c74217d9e409f9ed4e334bb68bfee6e3
Danilo Valério da Silva <danilo_vs@hotmail.com>
2019-05-05 10:47:05 -0300

    Evento Adicionar e com bind de this passado por outro component

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -4,11 +4,23 @@ import TodoForm from './todoForm'
 import TodoList from './todoList'
 
 export default class Todo extends Component {
+    constructor(props){
+        super(props)
+        //fazendo o bind para trazer o this (e suas props) a partir do click lá em todoForm
+        this.handleAdd = this.handleAdd.bind(this)
+    }
+
+    //Função para lidar com Adicionar
+    handleAdd(){
+        console.log("This de handleAdd é: ", this)
+    }
+
     render(){
         return(
             <div>
                 <PageHeader name='Tarefas' small='Cadastro'/>
-                <TodoForm/>
+                {/* a props handleAdd está recebendo o valor de todoForm */}
+                <TodoForm handleAdd={this.handleAdd}/>
                 <TodoList/>
             </div>
         )
